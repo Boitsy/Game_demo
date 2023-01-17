239782fbf44d2c04326075fc74ee15285548417a
itu-itis22-ozturkde22 <ozturkde22@itu.edu.tr>
2023-01-17 14:47:59 +0300

    Updated the comments on the code

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,16 +11,19 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	#Vectors get normalized so that the character doesn't go faster when moving diagonally.
 	input_vector = input_vector.normalized()
  
 	if input_vector != Vector2.ZERO:
 		
-		#Get closer to maxspeed by adding acceleration every frame
+		#Get closer to maxspeed by adding acceleration every frame. Delta binds it to irl time instead of fps.
+		#this prevents the game slowing down because of lag etc.
 		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
 		
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
-	#Remembers the velocity after the collision
+	#The character stops completely if it hits a wall it can't slide across.
 	velocity = move_and_slide(velocity)
 	print(velocity)
