7c14e45e92e5145932a1177d00114c2d1695e5e1
itu-itis22-ozturkde22 <ozturkde22@itu.edu.tr>
2023-01-18 22:42:42 +0300

    Added animations to the character

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,10 @@ export var maxspeed: = 80
 const acceleration: = 500
 const friction: =  500
 
-# Called when the node enters the scene tree for the first time.
+#Won't initialize until the AnimationPlayer node is ready.
+onready var animation_player = $AnimationPlayer
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -17,11 +20,21 @@ func _physics_process(delta):
  
 	if input_vector != Vector2.ZERO:
 		
+		#Play the run_right animation when the input_vector is bigger than 0 or plays run_left animation 
+		if input_vector.x > 0:
+			animation_player.play("run_right")
+		elif input_vector.x < 0:
+			animation_player.play("run_left")
+		elif input_vector.y > 0:
+			animation_player.play("run_down")
+		else:
+			animation_player.play("run_up")
 		#Get closer to maxspeed by adding acceleration every frame. Delta binds it to irl time instead of fps.
 		#this prevents the game slowing down because of lag etc.
 		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
 		
 	else:
+		animation_player.play("idle_right")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
 	#The character stops completely if it hits a wall it can't slide across.
