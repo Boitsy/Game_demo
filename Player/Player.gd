9ad977e6f55b293319af88011d905ac029322785
itu-itis22-ozturkde22 <ozturkde22@itu.edu.tr>
2023-01-20 18:48:01 +0300

    Added AnimationTree to make animation transitions easier.Updated the comments accordingly.

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,7 +8,9 @@ const friction: =  500
 
 #Won't initialize until the AnimationPlayer node is ready.
 onready var animation_player = $AnimationPlayer
-
+onready var animation_tree = $AnimationTree
+#The parameters/playback can be found by hovering over playback on AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -19,22 +21,21 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
  
 	if input_vector != Vector2.ZERO:
+		#The parameters used in the "set" fucntion can be found by hovering over "Run" and "Idle" respectively.
+		#These functions set the animation blend positions to our input_vector.
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
 		
-		#Play the run_right animation when the input_vector is bigger than 0 or plays run_left animation 
-		if input_vector.x > 0:
-			animation_player.play("run_right")
-		elif input_vector.x < 0:
-			animation_player.play("run_left")
-		elif input_vector.y > 0:
-			animation_player.play("run_down")
-		else:
-			animation_player.play("run_up")
+		#Use the "Run" animations.
+		animation_state.travel("Run")
+		
 		#Get closer to maxspeed by adding acceleration every frame. Delta binds it to irl time instead of fps.
 		#this prevents the game slowing down because of lag etc.
 		velocity = velocity.move_toward(input_vector * maxspeed, acceleration * delta)
 		
 	else:
-		animation_player.play("idle_right")
+		#Use the "Idle" animations.
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
 	#The character stops completely if it hits a wall it can't slide across.
