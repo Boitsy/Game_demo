d5b0bf8c0b3716147decaadeba8d5bdd5000f4c4
itu-itis22-ozturkde22 <ozturkde22@itu.edu.tr>
2023-01-21 16:08:19 +0300

    Added attak animations for the player and added the needed attack animations to the AnimationTree as well, movement relocated to a function named move_state

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,13 +6,36 @@ export var maxspeed: = 80
 const acceleration: = 500
 const friction: =  500
 
+enum{
+	Move,
+	Roll,
+	Attack
+}
+var state = Move
+
 #Won't initialize until the AnimationPlayer node is ready.
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 #The parameters/playback can be found by hovering over playback on AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
+func _ready():
+	#When the game is ready activate AnimationTree.
+	animation_tree.active = true
+
 func _physics_process(delta):
+	match state:
+		Move:
+			move_state(delta)
+		
+		Roll:
+			pass
+		
+		Attack:
+			attack_state(delta)
+	
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,10 +44,11 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
  
 	if input_vector != Vector2.ZERO:
-		#The parameters used in the "set" fucntion can be found by hovering over "Run" and "Idle" respectively.
+		#The parameters used in the "set" function can be found by hovering over "Run" and "Idle" respectively.
 		#These functions set the animation blend positions to our input_vector.
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		
 		#Use the "Run" animations.
 		animation_state.travel("Run")
@@ -40,4 +64,13 @@ func _physics_process(delta):
 		
 	#The character stops completely if it hits a wall it can't slide across.
 	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("attack"):
+		state = Attack
 	
+	
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animation_state.travel("Attack")
+
+func attack_state_finished():
+	state = Move
